99bf47fc752bc7b5d508beb81a9e2f50f8976552
roquiles <roberta_btg@hotmail.com>
2022-05-02 13:07:36 -0300

    Include router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyles from "./styles/global";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <GlobalStyles />
       Hello World!
-    </div>
+    </BrowserRouter>
   );
 }
 
