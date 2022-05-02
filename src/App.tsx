30b9f556c3180f031a9eeaa28bb44d3e4ebfe22a
roquiles <roberta_btg@hotmail.com>
2022-05-02 11:36:58 -0300

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyles from "./styles/global";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <GlobalStyles />
+      Hello World!
+    </div>
+  );
 }
 
 export default App;
