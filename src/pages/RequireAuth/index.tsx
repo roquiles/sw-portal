2ab2cdf3247d15ba88bc05bbb178cc1aa1924658
roquiles <roberta_btg@hotmail.com>
2022-05-03 10:30:16 -0300

    Create authentication require page

---
+++ src/pages/RequireAuth/index.tsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../../Auth/AuthContext";
+
+export default function RequireAuth({ children }: { children: JSX.Element }) {
+  const auth = useContext(AuthContext);
+
+  if (!auth.isLogged) {
+    return <Navigate replace to="/login" />;
+  }
+
+  return children;
+}
