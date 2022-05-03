ec43b82bd90e88148bbaa9b083186fc10258e42f
roquiles <roberta_btg@hotmail.com>
2022-05-03 10:36:47 -0300

    Update app routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/index";
+import Films from "./pages/Films";
+import RequireAuth from "./pages/RequireAuth";
+import Home from "./pages/Home";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* <Route path="/" element={<Home />} exact /> */}
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/films"
+        element={
+          <RequireAuth>
+            <Films />
+          </RequireAuth>
+        }
+      />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
