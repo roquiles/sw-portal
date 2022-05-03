9cae758951a10661c0bea542840dba62c1f62ef1
roquiles <roberta_btg@hotmail.com>
2022-05-03 19:26:15 -0300

    Create poster mapper

---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+import episode1 from "../assets/posters/episode1.jpg";
+import episode2 from "../assets/posters/episode2.jpg";
+import episode3 from "../assets/posters/episode3.jpg";
+import episode4 from "../assets/posters/episode4.jpg";
+import episode5 from "../assets/posters/episode5.jpg";
+import episode6 from "../assets/posters/episode6.jpg";
+import { PosterMapperType } from "../types/types";
+
+export const posterMapper: PosterMapperType = {
+  episode1,
+  episode2,
+  episode3,
+  episode4,
+  episode5,
+  episode6,
+};
