1cb7a9c8e4d59ccb49f6c892e151e49a1e070c60
roquiles <roberta_btg@hotmail.com>
2022-05-04 23:21:25 -0300

    Updata film and resources types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,22 @@
 export interface Film {
   title: string;
   episode_id: number;
+  release_date: string;
+  director: string;
+  producer: string;
+  opening_crawl: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
 }
 
 export interface PosterMapperType {
   [key: string]: string;
+}
+
+export interface ResourceType {
+  name: string;
+  url: string;
 }
