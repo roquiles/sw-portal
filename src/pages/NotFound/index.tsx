68b958074a51075ce2fef9558ff1e859a1dbf3dd
roquiles <roberta_btg@hotmail.com>
2022-05-06 13:50:18 -0300

    Create 'not found' page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import Header from "../../Components/Header";
+import { TitleNotFound } from "./styles";
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <TitleNotFound>Page Not Found. (404)</TitleNotFound>
+    </>
+  );
+}
---
+++ src/pages/NotFound/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const TitleNotFound = styled.h1`
+  margin-top: 3rem;
+  text-align: center;
+`;
